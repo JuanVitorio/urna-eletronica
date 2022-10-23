43fb984616e03aa77ae3f4c5865f1c5559c90c18
Whoan10 <juanvitorioclash@gmail.com>
2022-10-23 15:21:27 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
 'use strict'
 
-var num1 = document.querySelector('#first-number')
-var num2 = document.querySelector('#second-number')
+function votar() {
+  let num1 = String(document.getElementById('num1').value)
+  let num2 = String(document.getElementById('num2').value)
 
-var numero = num1 + num2
 
-console.log(numero)
+
+  let voto = num1 + num2
+
+  if (Number(voto) == 12) {
+    artur()
+  } else if (Number(voto) == 30) {
+    mamae()
+  } else
+
+}
+
+function artur() {
+  document.querySelector('.inputs-visibles').classList.toggle('hide')
+  document.querySelector('.img-box').classList.toggle('hide')
+  document.querySelector('.expli').classList.toggle('hide')
+  document.querySelector('#nome-candidato').innerHTML = 'Turzin'
+  document.querySelector('#partido-candidato').innerHTML = 'ZK'
+  document.querySelector('.img-urna').src = '../img/turzin.jpg'
+  document.querySelector('#input-one').innerHTML = '1'
+  document.querySelector('#input-two').innerHTML = '2'
+}
+
+function mamae() {
+  document.querySelector('.inputs-visibles').classList.toggle('hide')
+  document.querySelector('.img-box').classList.toggle('hide')
+  document.querySelector('.expli').classList.toggle('hide')
+  document.querySelector('#nome-candidato').innerHTML = 'Claudercia Ferreira'
+  document.querySelector('#partido-candidato').innerHTML = 'LC'
+  document.querySelector('.img-urna').src = '../img/mamae.jpg'
+  document.querySelector('#input-one').innerHTML = '3'
+  document.querySelector('#input-two').innerHTML = '0'
+}
